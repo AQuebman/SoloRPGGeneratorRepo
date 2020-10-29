6b363d4a1a217cc79b3d4f210c47649c5d522494
Soshsman <AQuebman@gmail.com>
2020-10-29 03:28:57 -0400

    Initial Upload to Repo
    Initial upload onto git/github of the Solo RPG Generator project

---
+++ SOLO_RPG/Solo_RPG_Generation/SoloRPG.py
@@ -0,0 +1,142 @@
+from kivy.app import App
+
+#Builder allows us to call another file such as our kv file
+from kivy.lang import Builder
+
+#BoxLayout allows us to use this layout
+from kivy.uix.boxlayout import BoxLayout
+
+#Random allows us to use random choice and other random triggers
+import random
+
+#Allows us to trigger window size or make the app full screen
+from kivy.core.window import Window
+
+#Setting the size of the window that opens
+Window.fullscreen = 'auto'
+
+#Deciding if user can resize the window or not
+from kivy.config import Config
+Config.set('graphics', 'resizable', True)  
+
+#Importing a CSV
+import csv   
+
+#Imports setting up a button with Kivy
+from kivy.uix.button import Button
+
+#Imports setting up a popup with kivy
+from kivy.uix.popup import Popup
+
+#Imports the kivy layout of Grid
+from kivy.uix.gridlayout import GridLayout  
+        
+class Generators(BoxLayout):
+#Generates random LOUCHE choice for 7-9 rolls in Dungeon World  
+    def louche(self, *args):
+        LOUCHE = ('LOCALITY: Something specifically related to the current environment happens.\nThe buildings now on fire. The ground collapses. It\'s flooding. Moonquake!' , 'OFFER: Offer a bargain, an extra, or a perk for a cost.\noffer a better position, with risk. Offer a temptation.', 'UNEXPECTED DANGER: Make something up or roll it up at random.\nTie it in if you want now or worry about how it fits in later', 'CALLBACK: Use something that they\'ve given you. A backstory element.\nAn off-handed comment. Gear. A character sheet aspect', 'HARM: Deal damage', 'END SOMETHING: End an ongoing effect, bonus, or fictional advantage. Take a \nresource away, something you possess, whether it\'s a piece of gear, \nan ability, or an ally')
+        self.ids.main_label.text = (str(random.choice(LOUCHE)))
+
+
+#Female Name Generation
+    def Female_name_gen(self, *args):
+#Declares the array variables of First_Name and Last_Name
+        First_Name = []
+        Last_Name = []
+#Opens the csv file that contains the random names
+        with open(r'C:\Users\Aaron\Desktop\Names_List.csv') as f:
+            reader = csv.reader(f, skipinitialspace=True)         
+            for col in reader: 
+                #This verifies it doesn't get a blank or the column title and if it does it continues until it doesn't get a blank
+                if not col[1] or col[1] == "Female First Name": continue
+                else:
+                #This appends column 1 to First_Name
+                    First_Name.append(col[1])
+                #This verifies it doesn't get a blank or the column title and if it does it continues until it doesn't get a blank
+                if not col[2]: continue
+                if not col[2] or col[2] == "Last Names": continue
+                else:
+                #This appends column 2 to Last_Name
+                    Last_Name.append(col[2])
+                
+#Print a random first and last name for females
+                self.ids.Char_Name.text = 'Name: ' + random.choice(First_Name) + ' ' + random.choice(Last_Name)    
+                                      
+#Male Name Generation               
+    def Male_name_gen(self, *args):
+        First_Name = []
+        Last_Name = []
+        with open(r'C:\Users\Aaron\Desktop\Names_List.csv') as f:
+            reader = csv.reader(f, skipinitialspace=True)         
+            for col in reader: 
+                ##This verifies it doesn't get a blank or the column title and if it does it continues until it doesn't get a blank
+                if not col[0] or col[0] == "Male First Name": continue
+                else:
+                #This appends column 0 to First_Name
+                    First_Name.append(col[0])
+                ##This verifies it doesn't get a blank or the column title and if it does it continues until it doesn't get a blank
+                if not col[2] or col[2] == "Last Names": continue
+                else:
+                #This appends column 2 to Last_Name
+                    Last_Name.append(col[2])
+
+#Print a random first and last name for Males
+        self.ids.Char_Name.text = 'Name: ' + random.choice(First_Name) + ' ' + random.choice(Last_Name)        
+
+#Clears what is showing on the main_label of the application     
+    def clear(self, *args):
+        self.ids.main_label.text = ''
+        self.ids.Char_Name.text = ''
+        self.ids.class_label.text = ''
+ 
+#Generic Character Generator
+    def Gen_Char_Gen(self, *args): 
+        self.onButtonPress()
+        Class_Choice = ('Barbarian', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Sorcerer', 'Wizard')
+        self.ids.class_label.text = 'Class: ' + (str(random.choice(Class_Choice)))
+        
+
+    # On button press - Create a popup dialog with a label and a close button
+
+    def onButtonPress(self):
+
+        layout      = GridLayout(cols=1, padding=10)
+
+       
+
+        MaleNameButton  = Button(text  = "Click for Male Name Generation")
+
+        FemaleNameButton = Button(text = "Click for Female Name Generation")
+
+ 
+
+        layout.add_widget(MaleNameButton)
+
+        layout.add_widget(FemaleNameButton)       
+
+   
+
+        # Instantiate the modal popup and display
+
+        popup = Popup(title='Random Name Generator',
+
+                      content=layout)  
+
+        popup.open()   
+
+       
+#Need a close for both buttons and send them to different name generators based on gender
+        # Dismiss causes the popup to go away. Release generates the call to the name generator specified
+        MaleNameButton.bind(on_press=popup.dismiss)
+        MaleNameButton.bind(on_release = self.Male_name_gen) 
+        FemaleNameButton.bind(on_press=popup.dismiss)
+        FemaleNameButton.bind(on_press = self.Female_name_gen)  
+        
+        
+        
+class app1(App):
+    def build(self):
+        return Builder.load_file('SoloRPG.kv')
+
+if __name__=="__main__":
+    app1().run()
